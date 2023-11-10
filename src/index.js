446f3f1cbc57a5496a1288cb8fdc6a199e9dcf51
karandcool <sainikaran06@gmail.com>
2023-11-10 18:02:10 +0530

    updated code

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import '../node_modules/font-awesome/css/font-awesome.min.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 // import store from './redux/store';
 import "./index.css";
 
@@ -18,7 +18,7 @@ import OrderCompleted from './pages/orderCompleted';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <Provider>
+    {/* <Provider> */}
       <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -39,6 +39,6 @@ root.render(
         <Route path="/design-customise/" element={<Customise />} />
 
       </Routes>
-    </Provider>
+    {/* </Provider> */}
   </BrowserRouter>
 );
